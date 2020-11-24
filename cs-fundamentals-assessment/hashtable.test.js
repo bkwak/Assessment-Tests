37990f199cadcdbc69be238f323f002e86ba7f58
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-11-23 16:28:24 -0800

    hashTable tests complete

--- cs-fundamentals-assessment/hashtable.test.js
+++ cs-fundamentals-assessment/hashtable.test.js
@@ -1,18 +1,26 @@
 const {HashTable, hashCode} = require('../src/main.js');
+const HashTableExt = require('../src/extension.js');
 
 const ht = new HashTable();
 
 ht.set("codesmith", true);
 ht.set("Los Angeles", "the best");
 ht.set("juniors", 36);
+ht.set("junior", 11);
 ht.set("january", {});   //will be same hash as juniors
 ht.set("seniors", null);
 
+//FOR EXTENSION
+//hash table will have 12 items in storage
+const htExtension = new HashTableExt();
+for (let i = 0; i <= 11; i += 1) {
+    htExtension.set(`a${i}`, i);
+}
+const originalIndex = hashCode("a2", htExtension.SIZE);
+const originalSize = htExtension.SIZE;
+let prevIndex;
+
 describe("Set Method", () => {
-
-    test("it uses hashCode to generate a hash key", () => {
-
-    });
 
     test("it uses either an object or a linked list to handle collisions", () => {
         const index = hashCode("codesmith", ht.SIZE);
@@ -24,17 +32,12 @@ describe("Set Method", () => {
         const index = hashCode("juniors", ht.SIZE);
 
         expect(typeof ht.storage[index]).toBe("object")
-        expect(ht.storage[index]).toHaveProperty("january");
-        expect(ht.storage[index]).toHaveProperty("juniors");
-
+        expect(ht.storage[index]).toHaveProperty("january", {});
+        expect(ht.storage[index]).toHaveProperty("juniors", 36);
     });
 });
 
 describe("Get Method", () => {
-
-    test("it uses hashCode to generate a hash key", () => {
-
-    });
 
     test("it works", () => {
         expect(ht.get("codesmith")).toBe(true);
@@ -49,22 +52,18 @@ describe("Get Method", () => {
 });
 
 describe("Remove Method", () => {
-    const returned = ht.remove("january");
-
-    test("it uses hashCode to generate a hash key", () => {
-
-    });
-
+    
     test("Deletes appropriate value set at hash key and original key", () => {
         const index = hashCode("juniors", ht.SIZE);
-
+        const returned = ht.remove("january");
+        
         expect(typeof ht.storage[index]).toBe("object")
-        expect(ht.storage[index]).toHaveProperty("january");
-        expect(ht.storage[index]).not.toHaveProperty("juniors");
+        expect(ht.storage[index]).not.toHaveProperty("january");
+        expect(ht.storage[index]).toHaveProperty("juniors");
+        expect(returned).toEqual({});
     });
 
     test("it returns deleted value", () => {
-        expect(returned).toEqual({});
         expect(ht.remove("codesmith")).toBe(true);
         expect(ht.remove("juniors")).toBe(36)
     });
@@ -76,52 +75,42 @@ describe("Remove Method", () => {
 });
 
 describe("Extension", () => {
-    //hash table will have 12 items in storage
-    const htExtension = new HashTable();
-    for (let i = 0; i <= 12; i += 1) {
-        htExtension.set(`a${i}`, i);
-    }
-    const originalIndex = hashCode("a10", htExtension.SIZE);
-
-    describe("Set Method", () => {
-        //adding one more item should double the storage size
-        const originalSize = htExtension.SIZE;
-        htExtension.set("a13", 13);
-
+    
+    describe("Set Method", () => {     
         test("if current length is at 75% capacity of size property, it doubles the size", () => {
+            //adding one more item should double the storage size
+            htExtension.set("a11", 11);
             expect(htExtension.SIZE).toBe(originalSize * 2);
         });
 
         test("it rehash every element with new this.SIZE", () => {
-            newIndex = hashCode("a10", htExtension.SIZE);
-            
-            expect(oldIndex).not.toBe(newIndex);
-            expect(htExtension.storage[originalIndex]).not.toHaveProperty("a10");
-            expect(htExtension.storage[newIndex]).toHaveProperty("a10");
+            newIndex = hashCode("a2", htExtension.SIZE);
+            expect(originalIndex).not.toBe(newIndex);
+            expect(htExtension.storage[originalIndex]).not.toHaveProperty("a2");
+            expect(htExtension.storage[newIndex]).toHaveProperty("a2");
         });
 
     });
 
     describe("Remove Method", () => {
-        //hash table has 13 items, so need to remove 5 to bring to a total of 8 items in storage
-        for (let i = 13; i >= 9; i -= 1) {
-            htExtension.remove(`a${i}`);
-        }
-        const previSize = htExtension.SIZE;
-        const prevIndex = hashCode("a6", htExtension.SIZE);
-        //removing one more should trigger size to be halved
-        htExtension.remove('a8');
-
         test("If current SIZE is greater than 16 & length is at 25% capacity of size property, halve the size", () => {
-            expect(htExtension.SIZE).toBe(previousSize / 2);
+            //hash table has 13 items, so need to remove 5 to bring to a total of 8 items in storage
+            const prevSize = htExtension.SIZE;
+            prevIndex = hashCode("a5", htExtension.SIZE);
+            for (let i = 13; i >= 8; i -= 1) {
+                htExtension.remove(`a${i}`);
+            }
+            //removing one more should trigger size to be halved
+            htExtension.remove('a7');
+            expect(htExtension.SIZE).toBe(prevSize / 2);
         });
 
         test("it rehash every element with new this.SIZE", () => {
-            newIndex = hashCode("a6", htExtension.SIZE);
+            newIndex = hashCode("a5", htExtension.SIZE);
             
-            expect(oldIndex).not.toBe(newIndex);
-            expect(htExtension.storage[originalIndex]).not.toHaveProperty("a6");
-            expect(htExtension.storage[newIndex]).toHaveProperty("a6");
+            expect(prevIndex).not.toBe(newIndex);
+            if(prevIndex < htExtension.size) expect(htExtension.storage[prevIndex]).not.toHaveProperty("a5");
+            expect(htExtension.storage[newIndex]).toHaveProperty("a5");
         });
 
     });
