fb7453c452188441f8664d2273c7b62e4ba8ad61
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-11-29 04:07:44 -0800

    GET /fancy tests added

--- node-assessment/server.test.js
+++ node-assessment/server.test.js
@@ -49,4 +49,26 @@ describe("GET /that", () => {
     });
 });
     
+describe("GET /fancy", () => {
+    test('it sets the status to 200', async () => {
+        const response = await request.get('/fancy'); 
+        expect(response.status).toBe(200);
+    });
+
+    test('it sets the header of `Content-Type` to `text/html; charset=UTF-8`', async () => {
+        const response = await request.get('/fancy'); 
+        expect(response.headers['content-type']).toBe('text/html');
+        expect(response.headers['charset']).toBe('utf-8');
+    });
+
+    test('it serves the fancy.html page', async (done) => {
+        const response = await request.get('/fancy'); 
+        const file = response.text;
+        fs.readFile(path.join(__dirname,'../client/fancy.html'), (err, contents) => {
+            if(err) console.error(err);
+            expect(file).toBe(contents.toString());
+            done();
+        });
+    });
+});
     
