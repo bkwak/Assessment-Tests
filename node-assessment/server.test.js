d0a5c344486b1d120ce25d3b39bd81081d6c30dd
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-11-29 04:02:31 -0800

    headers test updated

--- node-assessment/server.test.js
+++ node-assessment/server.test.js
@@ -4,7 +4,11 @@ const request = supertest(app);
 
 
 describe("GET /this", () => {
-    
+    // request.serialize['text/html'] = (obj) => {
+    //     return 'html generated from obj';
+    // }
+
+
     test('it sets the status to 200', async () => {
         const response = await request.get('/this'); 
         expect(response.status).toBe(200);
@@ -12,12 +16,13 @@ describe("GET /this", () => {
 
     test('it sets the header of `Content-Type` to `text/html; charset=UTF-8`', async () => {
         const response = await request.get('/this'); 
-        console.log(response.headers);
+        expect(response.headers['content-type']).toBe('text/html');
+        expect(response.headers['charset']).toBe('utf-8');
     })
 
     test('it serves the this.html page', async () => {
         const response = await request.get('/this'); 
-        console.log(response.body);
+        console.log(response);
     })
 
 });
