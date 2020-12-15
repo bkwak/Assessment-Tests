fdbc75d32a54981f653541c0c00ee7fed86d3945
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-12-14 19:26:49 -0800

    formatting + comments

--- database-assessment/database.test.js
+++ database-assessment/database.test.js
@@ -26,10 +26,12 @@ const newStudent2 = {
 beforeAll(async () => {
     const url = 'mongodb+srv://student:ilovetesting@database-assessment.ivrqc.mongodb.net/database-assessment?retryWrites=true&w=majority';
     await mongoose.connect(url, { useNewUrlParser: true });
-    
+
+    //seed the database with test data
     await Student.create(newStudent1);
     await Student.create(newStudent2);
 
+    //verify that the entries were properly added
     const results = await Student.find({});
     console.log(results);
 });
@@ -40,29 +42,30 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
+
 describe('POST/student', () => {
     let result;
-
+    
     test('it must be a correctly set up POST route', async (done) => {
         const res = await request.post('/student').send(newStudent);
         result = res.body;
         expect(res.status).toBe(200);
         done();
     })
-
+    
     test('it correctly creates a new document in the database', async (done) => {
-        const document = await Student.findOne({firstName: 'Barack',lastName: 'Obama',age: 60});
+        const document = await Student.findOne({ firstName: 'Barack', lastName: 'Obama', age: 60 });
         expect(document).toMatchObject(newStudent);
         done();
     })
-
+    
     test('it must send newly created document back to client', () => {
-        if(Array.isArray(result)) expect(result[0]).toMatchObject(newStudent);
+        if (Array.isArray(result)) expect(result[0]).toMatchObject(newStudent);
         else if (Object.getPrototypeOf(result) === 'Object') expect(result).toMatchObject(newStudent);
     })
-
+    
     test('Proper error handling: the response must have status code 418', async (done) => {
-        const res = await request.post('/student').send({firstName: new Error()});
+        const res = await request.post('/student').send({ firstName: new Error() });
         expect(res.status).toBe(418);
         done();
     })
@@ -71,32 +74,32 @@ describe('POST/student', () => {
 describe('GET /student/:name', () => {
     test('it correctly retrieves document from the database', async (done) => {
         const res = await request.get('/student/Donald');
-        if(Array.isArray(res)) {
+        if (Array.isArray(res)) {
             expect(res.body).toHaveLength(1);
             expect(res.body[0]).toMatchObject(newStudent1);
         }
-        else if(Object.getPrototypeOf(res.body) === 'Object') expect(res.body).toMatchObject(newStudent1);
+        else if (Object.getPrototypeOf(res.body) === 'Object') expect(res.body).toMatchObject(newStudent1);
         done();
     });
 });
 
 describe('PATCH /student/:name', () => {
     test('it  Updates document in the database correctly', async (done) => {
-        const newerStudent = {firstName: 'Donaldino'};
+        const newerStudent = { firstName: 'Donaldino' };
         await request.patch('/student/Donald').send(newerStudent);
-        const result = await Student.findOne({firstName: 'Donaldino'});
-        expect(result).toMatchObject({...newStudent1, ...newerStudent});
+        const result = await Student.findOne({ firstName: 'Donaldino' });
+        expect(result).toMatchObject({ ...newStudent1, ...newerStudent });
         done();
     });
 });
 
 describe('DELETE /student/:name', () => {
     test('Uses name to delete document from database', async (done) => {
-        let result = await Student.findOne({firstName: 'someone'});
+        let result = await Student.findOne({ firstName: 'someone' });
         expect(result).toBeTruthy();
-        
+
         await request.delete('/student/someone');
-        result = await Student.findOne({firstName: 'someone'});
+        result = await Student.findOne({ firstName: 'someone' });
         expect(result).toBeNull();
         done();
     });
